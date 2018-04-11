2417b50b943d3ff8685b6163dc4f7e7ae447b8d8
Edward H <eddy.hey@outlook.com>
2018-04-11 17:11:17 +0100

    add regex sub and superscript replacement

--- cogs/equations.py
+++ cogs/equations.py
@@ -12,6 +12,18 @@ keywords = {
     '[to]': '→'
 }
 
+superscriptDict = {
+    '1': '¹', '2': '²', '3': '³', '4': '⁴', '5': '⁵',
+    '6': '⁶', '7': '⁷', '8': '⁸', '9': '⁹', '0': '⁰'
+}
+
+subscriptDict = {
+    '1': '₁', '2': '₂', '3': '₃', '4': '₄', '5': '₅',
+    '6': '₆', '7': '₇', '8': '₈', '9': '₉', '0': '₀'
+}
+
+
+
 class Equation():
     def __init__(self, bot):
         self.bot = bot
@@ -20,11 +32,31 @@ class Equation():
     async def equation(self, ctx, *args):
         inp = ' '.join(args)
 
+        # Replace sup() with superscript
+        inp = regreplace(inp, '(sup\(.+\))', superscriptDict)
+
+        # Replace sup() with superscript
+        inp = regreplace(inp, '(sub\(.+\))', subscriptDict)
+
         # replace keywords with their special symbols.
         for word, replace in keywords.items():
             inp = inp.replace(word, replace)
 
         await ctx.send(inp)
+
+    def regreplace(inp, reg, rdict):
+        r = re.compile(reg)
+        results = r.finditer(inp)
+
+        for result in results:
+            match = result.group()[4:-1]
+            for char in match:
+                if char in rdict:
+                    match = match.replace(char, rdict[char])
+
+            inp = inp.replace(result.group(), match)
+
+        return inp
 
 def setup(bot):
     bot.add_cog(Equation(bot))
